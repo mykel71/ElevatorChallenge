4ac3b5aca8a6de381937c25dee8c41a96a4aa8f0
Michael Shepherd M <94452225+mykel71@users.noreply.github.com>
2023-07-24 23:07:17 +0200

    Add files via upload

---
+++ ElevatorChallenge.Test/Model/ElevatorSystemTests.cs
@@ -0,0 +1,81 @@
+﻿using ElevatorChallenge.Model;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge.Test.Model
+{
+    public class ElevatorSystemTests
+    {
+        [Fact]
+        public void GetNearestElevator_Should_ReturnNearestElevator()
+        {
+            // Arrange
+            int currentFloor = 3;
+            int numPeopleWaiting = 2;
+            int maxFloor = 10;
+            int elevatorSizeLimit = 5;
+            int weightLimit = 500;
+
+            var elevatorSystem = new ElevatorSystem(3, maxFloor, elevatorSizeLimit, weightLimit);
+
+            // Act
+            var nearestElevator = elevatorSystem.GetNearestElevator(currentFloor, numPeopleWaiting);
+
+            // Assert
+            Assert.NotNull(nearestElevator);
+        }
+
+        [Fact]
+        public void GetNearestElevator_NoAvailableElevator()
+        {
+            // Arrange
+            int currentFloor = 3;
+            int numPeopleWaiting = 6;
+            int maxFloor = 10;
+            int elevatorSizeLimit = 5;
+            int weightLimit = 500;
+
+            var elevatorSystem = new ElevatorSystem(2, maxFloor, elevatorSizeLimit, weightLimit);
+
+            // Act
+            var nearestElevator = elevatorSystem.GetNearestElevator(currentFloor, numPeopleWaiting);
+
+            // Assert
+            Assert.Null(nearestElevator);
+        }
+
+        [Fact]
+        public void GetNearestElevator_ElevatorIsMoving()
+        {
+            // Arrange
+            int currentFloor = 3;
+            int numPeopleWaiting = 2;
+            int maxFloor = 10;
+            int elevatorSizeLimit = 5;
+            int weightLimit = 500;
+
+            // Mock the random number generator for the elevator's initial floor
+            var randomMock = new Mock<Random>();
+            randomMock.Setup(r => r.Next(1, maxFloor + 1)).Returns(currentFloor);
+
+            var elevatorSystem = new ElevatorSystem(2, maxFloor, elevatorSizeLimit, weightLimit);
+
+            // Get the first available elevator (index 0)
+            var firstElevator = elevatorSystem.GetNearestElevator(currentFloor, numPeopleWaiting);
+            Assert.NotNull(firstElevator);
+
+            // Set the first elevator's destination floor to be different from the current floor (simulating elevator movement)
+            firstElevator.DestinationFloor = currentFloor + 2;
+
+            // Try to get another nearest elevator
+            var secondNearestElevator = elevatorSystem.GetNearestElevator(currentFloor, numPeopleWaiting);
+
+            // Assert
+            Assert.NotNull(secondNearestElevator);
+        }
+    }
+}
---
+++ ElevatorChallenge.Test/Model/UserInputTests.cs
@@ -0,0 +1,44 @@
+﻿using ElevatorChallenge.Interface;
+using ElevatorChallenge.Model;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge.Test.Model
+{
+    public class UserInputTests
+    {
+        [Theory]
+        [InlineData("5", 5)]
+        public void GetPositiveIntegerInput_ValidInput_ReturnsCorrectValue(string input, int expectedValue)
+        {
+            // Arrange
+            string prompt = "Enter a positive integer: ";
+
+            // Act
+            int result = UserInput.GetPositiveIntegerInput(input);
+
+            // Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData("3", 5, 2)]
+        public void GetDestinationFloor_ValidInput_ReturnsCorrectValue(string destinationFloorInput, int maxFloor, int expectedDestinationFloor)
+        {
+            // Arrange
+            int currentFloor = 2;
+            int elevatorId = 1;
+            var elevator = new Elevator(elevatorId, currentFloor, maxFloor);
+
+            // Act
+            int destinationFloor = UserInput.GetDestinationFloor(currentFloor, elevator);
+
+            // Assert
+            Assert.Equal(expectedDestinationFloor, destinationFloor);
+        }
+    }
+}
