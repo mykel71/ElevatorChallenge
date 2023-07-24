992f7cb734a0cd5326eb65f4c20d6571d7b3ac4b
Michael Shepherd M <94452225+mykel71@users.noreply.github.com>
2023-07-24 23:04:58 +0200

    Add files via upload

---
+++ ElevatorChallenge.Test/Mocks/MockUserInputProvider.cs
@@ -0,0 +1,40 @@
+﻿using ElevatorChallenge.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge.Test.Mocks
+{
+    public class MockUserInputProvider : IUserInputProvider
+    {
+        private int[] inputValues;
+        private int currentIndex = 0;
+
+        public MockUserInputProvider(params int[] inputValues)
+        {
+            this.inputValues = inputValues;
+        }
+
+        public int GetPositiveIntegerInput(string prompt)
+        {
+            return inputValues[currentIndex++];
+        }
+
+        public int GetNumPeopleWaiting(int maxCapacity, int weightLimit)
+        {
+            return 0;
+        }
+
+        public int GetFloorNumber(int maxFloor)
+        {
+            return 0;
+        }
+
+        public bool GetDirection()
+        {
+            return false;
+        }
+    }
+}
